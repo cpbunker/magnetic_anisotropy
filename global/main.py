fd8353ec0a82bb2dc0011126d060e936f372ba1a
cpbunker <christian@bunkerplanet.com>
2025-01-13 12:49:49 -0800

    modify main.py

--- global/main.py
+++ global/main.py
@@ -10,7 +10,7 @@ def find_global_directions():
     myjob.print_dirs_and_configs(local_ref_frame=False)
     #print("Opposite direction: {:6.1f} {:6.1f}".format(*opposite_direction))
 
-def do(direction_emin = (0.0, 0.0), e_ref = 0.0, max_energy = np.inf, de0 = 1.e-8):
+def do(to_do, e_ref, de0, direction_emin = (0.0, 0.0), max_energy = np.inf):
 
     myjob = vasp_jobs_ncl(n_theta=1801, n_phi=3600)
     #myjob.setup_local_ref_frames(z_direction=direction_emin, from_file=True)
@@ -28,37 +28,49 @@ def do(direction_emin = (0.0, 0.0), e_ref = 0.0, max_energy = np.inf, de0 = 1.e-
     #myjob.add_phis(theta=90, phi_min=315, phi_max=345, nphi=7, local_ref_frame=True)
     #myjob.add_thetas(phi=330, theta_min=75, theta_max=105, ntheta=7, local_ref_frame=True)
 
-    myjob.setup_local_ref_frames(z_direction=direction_emin, from_file=False)
+    #myjob.setup_local_ref_frames(z_direction=direction_emin, from_file=False)
     #myjob.add_thetas(phi=180, theta_min=6, theta_max=0, ntheta=4, local_ref_frame=True)
     #myjob.add_thetas(  phi=0, theta_min=0, theta_max=6, ntheta=4, local_ref_frame=True)
     #myjob.add_thetas(phi=270, theta_min=6, theta_max=0, ntheta=4, local_ref_frame=True)
     #myjob.add_thetas( phi=90, theta_min=0, theta_max=6, ntheta=4, local_ref_frame=True)
 
+    # sampling the 3 great circles of on the unit sphere
     myjob.add_thetas(phi=0, theta_min=0, theta_max=180, ntheta=7, local_ref_frame=True)
     myjob.add_thetas(phi=180, theta_min=0, theta_max=180, ntheta=7, local_ref_frame=True)
     myjob.add_thetas(phi=90, theta_min=0, theta_max=180, ntheta=7, local_ref_frame=True)
     myjob.add_thetas(phi=270, theta_min=0, theta_max=180, ntheta=7, local_ref_frame=True)
     myjob.add_phis(theta=90, phi_min=0, phi_max=330, nphi=12, local_ref_frame=True)
 
+    # the only usage I see of the reference energy is in
+    # if abs(e - e_ref) < max_energy: proceed as normal
+    # type statements
     myjob.e_ref = e_ref
+    myjob.print_dirs_and_configs(local_ref_frame=False)
 
-    #myjob.print_dirs_and_configs(local_ref_frame=True)
-    #myjob.print_dirs_and_configs(local_ref_frame=False)
-    #myjob.setup_jobs(submit=False)
-    #myjob.setup_jobs(submit=True)
-    #myjob.check_convergences(restart=False, de0=de0)
-    #myjob.check_convergences(restart=True, de0=de0)
-    #myjob.check_local_magmoms_all_configurations()
-    #restart(myjob, test=True, max_energy=max_energy)
-    #restart(myjob, test=False, max_energy=max_energy)
-    myjob.get_energies(max_energy=max_energy, de0=de0)
-    #get_all_energies(myjob, max_energy=max_energy, de0=de0)
+    #### execute these functions one at a time
+    ####
+
+    # submit=False creates input files but does not submit jobs so we can manually check
+    if(to_do==1):
+       myjob.setup_jobs(submit=False);
+
+    # start by running ONLY THE 0th FOLDER to get an existing WAVECAR that the other folders can reference
+    elif(to_do==2):
+        myjob.setup_jobs(submit=True, truncate_n_directions=1);
+
+    # finally submit all jobs
+    elif(to_do==3):
+        myjob.setup_jobs(submit=True, truncate_n_directions=1e10);
+
+    # check whether jobs converged and grab final energy of each job -> root_dir/energies.dat
+    elif(to_do==4):
+        myjob.check_convergences(restart=False, de0=de0)
+        myjob.get_energies(max_energy=max_energy, de0=de0)
+
+    else: raise Exception("No action for to_do = {:.0f}".format(to_do));
 
 if __name__ == "__main__":
 
-    #find_global_directions()
-
-    do(direction_emin = (98.282, 330.142), e_ref = -1834.36333292, max_energy = np.inf, de0 = 1.e-8)
-
-    pass
-
+    convergence_threshhold = 1.e-8;
+    reference_energy = -397.66719288;
+    do(sys.argv[3], reference_energy, convergence_threshold, direction_emin = (98.282, 330.142)); # no idea where these numbers are coming from
