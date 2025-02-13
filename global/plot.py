636d2dd990c2ca1e9265d21392858ae5b11bb089
Christian Bunker <christian@bunkerplanet.com>
2025-02-12 21:11:31 -0500

    Plotting classical energies in molecular plane

--- global/plot.py
+++ global/plot.py
@@ -9,6 +9,8 @@ import matplotlib as mpl
 eV2wavenumber = 8065.61;
 eV2meV = 1e3;
 plt.rcParams.update({"font.family":"serif"});
+plt.rcParams.update({"font.family":"Times New Roman"});
+plt.rcParams.update({"font.size":12});
 
 def polar_plot(base_name = "energies", title="", columns=(0, 1, 6), factor=1.0, wavenumber=True):
     '''
@@ -55,6 +57,7 @@ def polar_plot(base_name = "energies", title="", columns=(0, 1, 6), factor=1.0, 
     emin = np.nanmin(energies[:, column3])
     emax = np.nanmax(energies[:, column3])
 
+    # convert theta (column 1) and phi (column 2) to degrees
     energies[:, column1] = np.deg2rad(energies[:, column1])
     energies[:, column2] = np.deg2rad(energies[:, column2])
 
@@ -65,9 +68,6 @@ def polar_plot(base_name = "energies", title="", columns=(0, 1, 6), factor=1.0, 
     fig = plt.figure(figsize=(4.2, 3.2))
     ax = fig.add_subplot(projection="polar")
     
-    mpl.rcParams["font.family"] = "Times New Roman"
-    mpl.rcParams["font.size"] = 12
-    
     ax.set_title(title, {'fontsize': 13})
     
     ax.set_xlim([0, 2*np.pi])
@@ -76,8 +76,8 @@ def polar_plot(base_name = "energies", title="", columns=(0, 1, 6), factor=1.0, 
     ax.set_xticks(np.linspace(0, 2*np.pi, 9, endpoint=True), labels=[r"$\phi=0$", r"", r"$\phi=\pi/2$", r"", r"$\phi=\pi$", r"", r"$\phi=3\pi/2$", r"", r""])
     ax.set_yticks(np.linspace(0,   np.pi, 7, endpoint=True), labels=[r"$\theta=0$", r"", r"", r"$\theta=\pi/2$", r"", r"", r"$\theta=\pi$"])
 
+    # scatter phi, theta, energy points (use c=energy)
     cm = mpl.colormaps['rainbow']
-    
     scat = ax.scatter(energies[:,column2], energies[:,column1], s=60, c=energies[:,column3], vmin=emin, vmax=emax, cmap=cm)
     
     if(wavenumber):
--- mag_dip.py
+++ mag_dip.py
@@ -25,12 +25,43 @@ Joule2meV = 6.241509074460763e+21
 meV2wavenumber = 8.06554393738
 Joule2wavenumber = 5.034116567561925e+22 # Joule2meV * meV2wavenumber
 Ang2met = 1e-10;
+met2Ang = 1e10;
 Hz2microeV = 4.136e-9
 Hz2GHz = 1e-9;
 meV2microeV = 1e3;
 
 # Spin operators for S = 1/2
 S = [0.5*np.array([[0,1],[1,0]]), 0.5*np.array([[0,-1j],[1j,0]]), 0.5*np.array([[1,0],[0,-1]])]
+
+def set_z_axis(atoms):
+    '''
+    Sets the z axis of the LAB frame along the Cu-Cu direction
+    '''
+    p1 = atoms[0].position
+    p2 = atoms[1].position
+
+    ez = p2 - p1
+    ez = ez / np.linalg.norm(ez)
+
+    x = np.dot(ez, [1, 0, 0])
+    if abs(x) < 0.999:
+        ex = np.cross([1, 0, 0], ez)
+    else:
+        ex = np.cross([0, 1, 0], ez)
+    ex = ex / np.linalg.norm(ex)
+
+    ey = np.cross(ez, ex)
+
+    emat = [ex, ey, ez]
+
+    positions = atoms.get_positions()
+    positions = np.transpose(positions)
+    positions = emat @ positions
+    positions = np.transpose(positions)
+
+    atoms.set_positions(positions)
+
+    return atoms
 
 def get_A(r1, r2, print_in_GHz = False):
     """
@@ -125,7 +156,7 @@ def get_diff_E_xyz(r1, r2):
         print("{:12.6f} cm^-1".format(dE_wavenumber))
         print()
 
-def get_diff_E_en(r1, r2, en1, en2):
+def get_diff_E_en(r1, r2, en1, en2, verbose=True):
     """
     Get the energy difference between |en1, en2> and |en1, -en2>.
     """
@@ -152,43 +183,92 @@ def get_diff_E_en(r1, r2, en1, en2):
     dE_microeV = dE_J * Joule2meV*meV2microeV
     dE_wavenumber = dE_J * Joule2wavenumber
 
-    print("The energy difference E_ud - E_uu is")
-    print("{:12.6f} GHz".format(dE_GHz))
-    print("{:12.6f} microeV".format(dE_microeV))
-    print("{:12.6f} cm^-1".format(dE_wavenumber))
-    print()
+    if(verbose):
+        print("The energy difference E_ud - E_uu is:")
+        print("{:12.6f} GHz".format(dE_GHz))
+        print("{:12.6f} microeV".format(dE_microeV))
+        print("{:12.6f} cm^-1".format(dE_wavenumber))
+        print()
 
-def set_z_axis(atoms):
+def get_E_classical(r1, r2, en1, en2, verbose=True):
     '''
-    Sets the z axis of the LAB frame along the Cu-Cu direction
+    r1 and r2 are position vectors, in meters
     '''
-    p1 = atoms[0].position
-    p2 = atoms[1].position
+    for en in [en1, en2]:
+        if(abs(1-np.linalg.norm(en)) > 1e-10): raise ValueError;
 
-    ez = p2 - p1
-    ez = ez / np.linalg.norm(ez)
+    # r vector
+    rnorm = np.linalg.norm(r2-r1); # <- in meters
+    rA = rnorm*met2Ang;            # <- in angstroms
+    rhat = (r2-r1)/rnorm;
 
-    x = np.dot(ez, [1, 0, 0])
-    if abs(x) < 0.999:
-        ex = np.cross([1, 0, 0], ez)
+    # potential energy
+    classical_prefactor = -(53.69/Hz2microeV)/(rA**3); # <- in Hz
+    dE_Hz = classical_prefactor*(3*np.dot(rhat, en1)*np.dot(rhat, en2) - np.dot(en1, en2)); # <- still in Hz
+    if(verbose):
+        print("The classical magnetic dipole interaction energy is:")
+        print("{:12.6f} GHz".format(dE_Hz*Hz2GHz))
+        print("{:12.6f} microeV".format(dE_Hz*Hz2microeV))
+        print("{:12.6f} cm^-1".format(dE_Hz * h * Joule2wavenumber))
+        print()
+    return dE_Hz;
+
+def circular_E_classical(r1, r2, en1_init, en2, plane_definer, num_alpha=12, wavenumber = False):
+    '''
+    '''
+    import matplotlib.pyplot as plt
+    plt.rcParams.update({"font.family":"serif"});
+    plt.rcParams.update({"font.family":"Times New Roman"});
+    plt.rcParams.update({"font.size":12});
+
+    # set up coordinate system for the molecular plane
+    exprime = en1_init/np.linalg.norm(en1_init);
+    eyprime = plane_definer - np.dot(plane_definer,exprime)*exprime;
+    eyprime = eyprime/np.linalg.norm(eyprime);
+    print("exprime dot eyprime = {:.6f}".format(np.dot(exprime, eyprime)));
+
+    # rotate en1 around the molecular plane
+    E_class_vals = np.zeros((num_alpha),dtype=float);
+    alpha_vals = np.linspace(0, 2*np.pi, num_alpha+1)[:-1];
+    theta_fixed = np.pi/2*np.ones_like(alpha_vals); # keeps us in the xy plane of the polar projection,
+                                                    # which is the exprime-eyprime plane
+
+    for vali in range(num_alpha):
+        en1_alpha = exprime*np.cos(alpha_vals[vali]) + eyprime*np.sin(alpha_vals[vali]);
+        E_class_vals[vali] = get_E_classical(r1, r2, en1_alpha, en2, verbose=False); # <- in Hz
+        #print("E(rA={:3.2f}".format(np.linalg.norm(r2-r1)));
+        #print("alpha={:.2f}*pi) ".format( alpha_vals/np.pi));
+        #print("{:.6f} micro eV".format( Hz2microeV*E_class_vals[vali]));
+        
+        print("E(rA={:3.2f}, alpha={:.2f}*pi) = {:.6f} micro eV".format(np.linalg.norm(r2-r1)*met2Ang,
+                                                                alpha_vals[vali]/np.pi, Hz2microeV*E_class_vals[vali]));
+
+    # convert E_class_vals units
+    if(wavenumber): E_class_vals = E_class_vals * h * Joule2wavenumber; # < in cm^-1
+    else:           E_class_vals = E_class_vals * Hz2microeV;           # <- in micro eV
+ 
+    # polar plot
+    fig = plt.figure(figsize=(4.2, 3.2))
+    ax = fig.add_subplot(projection="polar")
+    ax.set_xlim([0, 2*np.pi])
+    ax.set_ylim([0, np.pi])
+    ax.set_xticks(np.linspace(0, 2*np.pi, 9, endpoint=True), labels=[r"$\phi=0$", r"", r"$\phi=\pi/2$", r"", r"$\phi=\pi$", r"", r"$\phi=3\pi/2$", r"", r""])
+    ax.set_yticks(np.linspace(0,   np.pi, 7, endpoint=True), labels=[r"$\theta=0$", r"", r"", r"$\theta=\pi/2$", r"", r"", r"$\theta=\pi$"])
+
+    # scatter phi, theta, energy points (use c=energy)
+    cm = plt.colormaps['rainbow']
+    scat = ax.scatter(alpha_vals, theta_fixed, c=E_class_vals,  
+                      s=60, vmin=np.nanmin(E_class_vals), vmax=np.nanmax(E_class_vals), cmap=cm)
+    
+    if(wavenumber):
+        cbar = plt.colorbar(scat, label = r"$E(\theta, \phi)\; (\mathrm{cm}^{-1})$", pad=0.15)
     else:
-        ex = np.cross([0, 1, 0], ez)
-    ex = ex / np.linalg.norm(ex)
+        cbar = plt.colorbar(scat, label = r"$E(\theta, \phi)\; (\mathrm{\mu eV})$", pad=0.15)
 
-    ey = np.cross(ez, ex)
+    fig.tight_layout()
+    plt.show();
 
-    emat = [ex, ey, ez]
-
-    positions = atoms.get_positions()
-    positions = np.transpose(positions)
-    positions = emat @ positions
-    positions = np.transpose(positions)
-
-    atoms.set_positions(positions)
-
-    return atoms
-
-def do_dimers(vaspfiles, whichselect=7):
+def do_dimers(vaspfiles, whichselect):
     '''
     For DHP,    use whichselect=4 to select N5, the correct N relative to Cu2
     For tbutyl, use whichselect=7 to select N8 
@@ -212,7 +292,9 @@ def do_dimers(vaspfiles, whichselect=7):
         en1 = en1 / np.linalg.norm(en1)
         en2 = Nselected - Cu2; 
         en2 = en2 / np.linalg.norm(en2)
-        outofplane = np.cross( (Cu1-Cu2)/np.linalg.norm(Cu1-Cu2) , en1);
+        # uses another Cu-N bond of 1st Cu to define the molecular plane
+        defines_inplane = (atoms[whichatomstart+1].position - Cu1)/np.linalg.norm(atoms[whichatomstart+1].position - Cu1);
+        outofplane = np.cross( en1, defines_inplane); # uses 
         outofplane = outofplane / np.linalg.norm(outofplane);
         print("en1 dot en2 = {:.2f}".format(np.dot(en1,en2)));
         print("en1 outofplane = {:.2f}".format(np.dot(en1,outofplane)));
@@ -220,6 +302,8 @@ def do_dimers(vaspfiles, whichselect=7):
         if( abs(np.dot(en1,en2)) < 0.5):
             print("en1 dot en2 = {:.2f}".format(np.dot(en1,en2)));
             raise Exception("change argument whichatom to better align quantization axes")
+        
+        # tell the user the locations and the quantum energy
         print("The location of metal 1 is r1 = ", Cu1, "Ang")
         print("The location of metal 2 is r2 = ", Cu2, "Ang")
         print("The metal-metal separation is {:.2f} Ang".format(np.linalg.norm(Cu1-Cu2)))
@@ -227,7 +311,14 @@ def do_dimers(vaspfiles, whichselect=7):
         print("The quantization axis for S2 (for vanadium2) is = ", en2)
         print()
         get_diff_E_en(Cu1*Ang2met, Cu2*Ang2met, en1, en2)
-        print("\n")
+        print()
+
+        # tell the user the classical energy
+        get_E_classical(Cu1*Ang2met, Cu2*Ang2met, en1, en2);
+
+        # plot the classical energy as the Cu1 spin rotates in its molecular plane
+        circular_E_classical(Cu1*Ang2met, Cu2*Ang2met, en1, en2, defines_inplane)
+
 
 if __name__ == "__main__":
     # Reference result for r12 = 3 Ang: 1.9 GHz, 1.9 GHz, and -3.8 GHz in the diagonal matrix elements
@@ -236,18 +327,21 @@ if __name__ == "__main__":
         ref_amt = (27.5074-10.5561) # <- in ang, for DHP closed
         ref_amt = (28.2996-11.6003) # <- in ang, for DHP open
         print("\nExamining reference separation of {:.2f} Ang\n".format(ref_amt))
-        r1_ref = [0, 0, 0]
-        r2_ref = [0, 0, ref_amt*Ang2met]
+        r1_ref = np.array([0, 0, 0]);
+        r2_ref = np.array([0, 0, ref_amt*Ang2met]);
 
         # choose the quantization axes
         quant1_ref = np.array([0,0,1]);
         # E_ud - E_uu is very sensitive to this choice, see e.g.
         #quant1_ref = np.array([0,np.sqrt(1/2),np.sqrt(1/2)]);
-        quant2_ref = -1*quant1_ref;
+        quant2_ref = 1*quant1_ref; # try (-1)
         print("quant1_ref dot qant2_ref = {:.2f}".format(np.dot(quant1_ref, quant2_ref)));
 
-        # run reference energy
-        get_diff_E_en(r1_ref, r2_ref, quant1_ref, quant1_ref);
+        # run reference energy - quantum
+        get_diff_E_en(r1_ref, r2_ref, quant1_ref, quant2_ref);
+
+        # run reference energy - classical
+        get_E_classical(r1_ref, r2_ref, quant1_ref, quant2_ref)
         del ref_amt, r1_ref, r2_ref, quant1_ref, quant2_ref;
 
-    do_dimers(sys.argv[1:]);
+    do_dimers(sys.argv[2:], int(sys.argv[1]));
